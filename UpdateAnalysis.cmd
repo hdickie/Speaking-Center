5acb652fbcf2adc5cc4611c6ac902c4dcb543031
Hume Dickie <hdickie@dons.usfca.edu>
2018-01-21 19:27:50 -0800

    Automated push.

--- UpdateAnalysis.cmd
+++ UpdateAnalysis.cmd
@@ -3,12 +3,10 @@ SETLOCAL EnableDelayedExpansion
 SET me=%~n0
 SET parent=%~dp0
 set R_Script="C:\Program Files\R\R-3.4.2\bin\RScript.exe"
-cd src
 
 git pull
 
 ::re-render Driver.Rmd into liveanalysis.html
-cd src
 %R_Script% -e "rmarkdown::render('Driver.Rmd',output_file='C:/Users/Work/Desktop/Github/Speaking-Center/liveanalysis.html')"
 
 cd %parent%
