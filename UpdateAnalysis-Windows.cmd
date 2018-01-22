09a193a00a0396e3df38b5bfc860a7e0a619cf7d
Hume Dickie <hdickie@dons.usfca.edu>
2018-01-21 20:58:14 -0800

    Automated push.

--- UpdateAnalysis-Windows.cmd
+++ UpdateAnalysis-Windows.cmd
@@ -3,11 +3,13 @@ SETLOCAL EnableDelayedExpansion
 SET me=%~n0
 SET parent=%~dp0
 set R_Script="C:\Program Files\R\R-3.4.2\bin\RScript.exe"
+cd src
 
 git pull
 
-::re-render Driver.Rmd into liveanalysis.html
-%R_Script% -e "rmarkdown::render('Driver.Rmd',output_file='C:/Users/Work/Desktop/Github/Speaking-Center/liveanalysis.html')"
+::re-render Driver.Rmd into index.html
+cd src
+%R_Script% -e "rmarkdown::render('Driver.Rmd',output_file='C:/Users/Work/Desktop/Github/Senior-Project/index.html')"
 
 cd %parent%
 git add *
