e2f23ec0d8e4150fba0b80ae611b94536644a29c
Hume Dickie <hdickie@dons.usfca.edu>
2018-01-21 19:15:20 -0800

    Automated push.

---
+++ UpdateAnalysis.cmd
@@ -0,0 +1,21 @@
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion
+SET me=%~n0
+SET parent=%~dp0
+set R_Script="C:\Program Files\R\R-3.4.2\bin\RScript.exe"
+set log_folder="C:\Users\Hume Dickie\Desktop\Github\Speaking-Center\logs"
+cd src
+
+git pull
+
+::re-render Driver.Rmd into index.html
+cd src
+%R_Script% -e "rmarkdown::render('Driver.Rmd',output_file='C:/Users/Work/Desktop/Github/Senior-Project/index.html')"
+
+cd %parent%
+git add *
+git commit -m "Automated push."
+git push
+
+echo Finished Successfully.
+pause
