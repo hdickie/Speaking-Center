4c4b88cbe5cc437c7e5bb19464513e9cb178f15c
Hume Dickie <hdickie@dons.usfca.edu>
2018-01-21 19:27:10 -0800

    Automated push.

--- UpdateAnalysis.cmd
+++ UpdateAnalysis.cmd
@@ -3,7 +3,6 @@ SETLOCAL EnableDelayedExpansion
 SET me=%~n0
 SET parent=%~dp0
 set R_Script="C:\Program Files\R\R-3.4.2\bin\RScript.exe"
-set log_folder="C:\Users\Work\Desktop\Github\Speaking-Center\logs"
 cd src
 
 git pull
